1abae5fc4f4bdbc179c183cb9bbd3af78aec4190
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-11-15 15:13:06 +0100

    Added repo structure and started lab 1

---
+++ Lab_1/Chatsystem/src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+/**
+ * About this class:
+ *
+ * @author Alexander Lundqvist
+ * Created: 15.11.2022
+ */
+public class Main {
+    
+    // Default constructor
+    public Main () {
+    
+    }
+
+    /**
+     * Main method with unit testing for the class.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Unit tests go here
+    }
+
+}
