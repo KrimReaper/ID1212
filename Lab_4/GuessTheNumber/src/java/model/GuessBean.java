d33271a012e2b17c38422fd16df27e7484b52398
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2023-01-07 05:07:34 +0100

    Inital commit

---
+++ Lab_4/GuessTheNumber/src/java/model/GuessBean.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * TBD
+ *
+ * @author Alexander Lundqvist & Ramin Shojaei
+ */
+
+public class GuessBean {
+    
+    // Default constructor
+    public GuessBean() {
+    }
+
+    /**
+     * Main method with unit testing for the class.
+     * @param args takes no input arguments
+     */    
+    public static void main(String[] args) {
+        
+    }
+}
