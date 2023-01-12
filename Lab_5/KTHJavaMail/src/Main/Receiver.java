46f74f5fab10d0274560e93f6bce5391db7886b7
Raminking01 <71043231+Raminking01@users.noreply.github.com>
2023-01-12 05:15:14 +0100

    Commit Lab 5

---
+++ Lab_5/KTHJavaMail/src/Main/Receiver.java
@@ -0,0 +1,29 @@
+package Main;
+import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimePart;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import java.util.Scanner;
+
+
+/**
+ * TBD
+ *
+ * @author Alexander Lundqvist & Ramin Shojaei
+ */
+public class Receiver {
+    
+    // Default constructor
+    public Receiver() {
+    }
+
+    /**
+     * Main method with unit testing for the class.
+     * @param args takes no input arguments
+     */    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Lab_5/KTHJavaMail/src/Main/Sender.java
@@ -0,0 +1,73 @@
+package Main;
+
+import java.io.Console;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Message;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * TBD
+ *
+ * @author Alexander Lundqvist & Ramin Shojaei
+ */
+public class Sender {
+    private static String username;
+    private static String password;
+    
+
+
+    /**
+     * Main method with unit testing for the class.
+     * @param args takes no input arguments
+     */    
+    
+    //Insert your username and password
+    public static void main(String[] args) {
+        //Console console = System.console();
+        username = "rshojaei";
+                //console.readLine("Type your Username:  ");
+        password =  "Zlatan81";
+                //new String(console.readPassword("Your Password:  "));
+        
+        //Properties field is created
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.kth.se");
+        properties.put("mail.smtp.port", "587");
+
+//Authenticator represents an object that knows how to obtain authentication for a network connection
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+        //We create this authenticator to pass a username and password to its constructor.
+            protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password );
+                    }
+                });
+        
+            try {
+		// MimeMessage implements javax.mail.internet.MimePart
+                //It defines content of what we want to transfer
+                //To create a msg it's required to pass session object in Mime Message constructor
+		Message msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress(username + "@kth.se"));
+                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(username + "@kth.se"));
+                msg.setText("Hello, what's up?");
+	
+                //Uses the SMTP Protocol to send msg
+		Transport.send(msg);
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+    }
+    
+    
+    
+}
